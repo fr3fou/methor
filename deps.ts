adc32853cbc0dab04314d3146274467e68c7043c
Simo Aleksandrov <simo3003@me.com>
2020-10-31 00:58:03 +0200

    Init

---
+++ deps.ts
@@ -0,0 +1,1 @@
+export * from "https://raw.githubusercontent.com/fr3fou/djena/master/parse.ts";
---
+++ math.ts
@@ -0,0 +1,20 @@
+interface Expression {
+  expressionNode(): void;
+}
+
+class InfixExpression implements Expression {
+  constructor(
+    readonly operator: Operator,
+    readonly lhs: Expression,
+    readonly rhs: Expression
+  ) {}
+
+  expressionNode() {}
+}
+
+class Integer implements Expression {
+  constructor(readonly value: number) {}
+  expressionNode(): void {}
+}
+
+type Operator = "+" | "-" | "*" | "/";
